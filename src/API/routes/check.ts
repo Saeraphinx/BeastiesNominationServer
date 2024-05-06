f53b79f128ca40305ebeb8bf93220c00829b0001
Saeraphinx <saeraphinx@gmail.com>
2024-05-06 14:46:06 -0500

    add global ratelimit to endpoint

--- src/API/routes/check.ts
+++ src/API/routes/check.ts
@@ -3,14 +3,22 @@ import { DatabaseHelper } from '../../Shared/Database';
 
 export class TempRoute {
     private app: Express;
+    private recentConnections: string[] = [];
 
     constructor(app: Express) {
         this.app = app;
         this.loadRoutes();
+        setInterval(() => {
+            this.recentConnections = [];
+        }, 3000);
     }
 
     private async loadRoutes() {
         this.app.post(`/api/submitnomination`, async (req, res) => {
+            this.recentConnections.push(req.ip);
+            if (this.recentConnections.length > 5) {
+                return res.status(429).send(`Too many requests.`);
+            }
             const userId = req.body[`id`].toString();
             const bsrId = req.body[`bsrId`].toString();
             const category = req.body[`category`].toString();
