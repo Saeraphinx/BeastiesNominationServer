b9cf128f1dbc7b8957111c0a4596a0fef2465fa7
Saeraphinx <saeraphinx@gmail.com>
2024-11-11 16:51:42 -0600

    add webhook logger

--- src/Shared/Logger.ts
+++ src/Shared/Logger.ts
@@ -1,29 +1,29 @@
-//import { WebhookClient, EmbedBuilder, ColorResolvable, time, TimestampStyles } from "discord.js";
-//import { logging } from '../../storage/config.json';
+import { WebhookClient, EmbedBuilder, ColorResolvable, time, TimestampStyles } from "discord.js";
+import { logging, devmode } from '../../storage/config.json';
 
 
 export class Logger {
     //private static webhook:WebhookClient = new WebhookClient({id: logging.id, token: logging.token});
-    //private static webhook:WebhookClient = new WebhookClient({url: logging.webhookUrl});
+    private static webhook:WebhookClient;
     
     public static rawlog(message:any) {
         console.log(message);
-        //this.sendWebhookLog(message);
+        this.sendWebhookLog(message);
     }
 
     public static log(message:any, moduleName?:string) {
-        console.log(`[LUMA${moduleName ? ` ${moduleName}` : ``}] ${new Date(Date.now()).toLocaleString()} > ${message}`);
-        //this.sendWebhookLog(`[LUMA${moduleName ? ` ${moduleName}` : ``}] ${time(new Date(Date.now()), TimestampStyles.LongTime)} > ${message}`);
+        console.log(`[BNS${moduleName ? ` ${moduleName}` : ``}] ${new Date(Date.now()).toLocaleString()} > ${message}`);
+        this.sendWebhookLog(`[BNS${moduleName ? ` ${moduleName}` : ``}] ${time(new Date(Date.now()), TimestampStyles.LongTime)} > ${message}`);
     }
 
     public static warn(message:any, source?:string) {
         console.warn(`[WARN] ${new Date(Date.now()).toLocaleString()} > ${message}`);
-        //this.sendWebhookEmbed(`Warning`, message, 0xFF9900, source);
+        this.sendWebhookEmbed(`Warning`, message, 0xFF9900, source);
     }
 
     public static error(message:any, source?:string) {
         console.error(`[ERROR] ${new Date(Date.now()).toLocaleString()} > ${message}`);
-        //this.sendWebhookEmbed(`Error`, message, 0xFF0000, source);
+        this.sendWebhookEmbed(`Error`, message, 0xFF0000, source);
     }
 
     //compatibility
@@ -39,21 +39,26 @@ export class Logger {
         Logger.error(message, source);
     }
     
-    /*
+    
     private static sendWebhookEmbed(title:string, message:any, color:ColorResolvable, source:string) {
         let loggingEmbed:EmbedBuilder = new EmbedBuilder()
             .setTitle(title)
             .setDescription(message.toString())
             .setColor(color)
             .setFooter({
-                text: `Luma - ${source}`,
+                text: `BNS - ${source}`,
             })
             .setTimestamp();
 
-        Logger.webhook.send({embeds: [loggingEmbed], allowedMentions: { users: [`213074932458979330`], roles: []}});
+        if (!devmode) {
+            if (!Logger.webhook) { Logger.webhook = new WebhookClient({url: logging.webhookUrl}); }
+            Logger.webhook.send({embeds: [loggingEmbed], allowedMentions: { users: [`213074932458979330`], roles: []}});
+        }
     }
     private static sendWebhookLog(message:any) {
-        Logger.webhook.send({content: message, allowedMentions: { users: [], roles: []}}).catch(console.error);
+        if (!devmode) {
+            if (!Logger.webhook) { Logger.webhook = new WebhookClient({url: logging.webhookUrl}); }
+            Logger.webhook.send({content: message, allowedMentions: { users: [], roles: []}}).catch(console.error);
+        }
     }
-    */
 }
